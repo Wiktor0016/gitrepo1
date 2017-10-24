5edadf8fdd7072c6c05a247e9b0f01fcc15a8fea
Wiktor0016 <sobowiec@onet.pl>
2017-10-24 10:36:31 +0200

    zadania

--- cpp/tabele.cpp
+++ cpp/tabele.cpp
@@ -4,30 +4,54 @@
 
 #include <iostream>
 using namespace std;
+
+void ile5(int tab[], int ile)
+{
+    int i;
+    int licznik = 0;
+    int parzyste = 0;
+    for (i = 1; i < ile; i++)
+    {
+        if ( tab[i] % 5 == 0 )
+        licznik5++;
+        if (tab[i] % 2 == 0)
+        parzyste++;
+    }
+        cout << "Ilosc dzielnikow 5: " << licznik5 << endl;
+}
+void pobierzLiczby(int tab[], int ile)
+{   
+    int i = 0;
+    for (i = 0; i < ile; i++)    
+    {
+        cout << "Podaj liczbe: ";
+        cin >> tab[i];
+    }   
+}
+
+void najmniejsza(int tab[], int ile)   
+{
+    int min = tab[0];
+    int i = 0;
+    for (i = 1; i < ile; i++)
+    {    
+    if (min > tab[i])
+    min = tab[i];
+    cout << "Najmniesza liczba:" << min << endl;
+    }
+}
 int main(int argc, char **argv)
 
 {
-    
-    int liczby[10];
-    int i = 0;
-    int suma = 0;
-    int licznik = 0;
-    
-    
-    for (int i = 0; i < 10; i++)
-    {
-        cout << "Podaj liczbe";
-        cin >> liczby[i];
-    }
-    
-    for (int i = 0 ; i < 10; i++)
-    {
-        //cout << liczby[i] << " " ;
-        suma += liczby[i];
-        if (liczby[i] % 2 == 0) licznik ++;
-    }
-        cout << "Suma liczb parzystych" << licznik << endl;
-        cout << "Suma liczb" << suma << endl;
+        int rozmiar = 0;
+        cout << "Ile liczb podasz:";
+        cin >> rozmiar;
+        
+        int liczby[rozmiar];
+
+        pobierzLiczby(liczby, rozmiar);
+        najmniejsza(liczby, rozmiar);
+        ile5(liczby, rozmiar);
     return 0;
 }
 
