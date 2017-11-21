6624208eb77dd4e6c91f3d13f953e56eec15db58
Wiktor0016 <sobowiec@onet.pl>
2017-11-21 10:35:46 +0100

    cw

--- python/euklides.py
+++ python/euklides.py
@@ -1,19 +1,29 @@
-def nww_v1(a, b):
+def nwd_v1(a, b):
+    """wersja klasyczna"""
     while a != b:
         if a > b:
-            a=a-b
+            a = a - b
         else:
-            b=b-a
+            b = b - a
     return a
 
-def main (args):
+
+def nwd_v2(a, b):
+    """wesja optymalna"""
+    while a > 0:
+        a = a % b
+        b = b - a
+    return b
+
+
+def main(args):
     a = int(input("Podaj liczbę naturalną: "))
     b = int(input("Podaj liczbę naturalną: "))
-    assert nww_v1(5, 10) == 5
-    assert nww_v1(3, 9) == 3
-    assert nww_v1(11, 33) == 11
-    print("NWW({:d}, {:d}) = {:d}".format(a, b, nww_v1(a, b)))
+    assert nwd_v2(5, 10) == 5
+    assert nwd_v2(11, 33) == 11
+    print("NWD({:d}, {:d}) = {:d}".format(a, b, nwd_v2(a, b)))
     return 0
+
 
 if __name__ == '__main__':
     import sys
