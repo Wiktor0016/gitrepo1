ec3b9c50746c9acbf11772827e4f8d423b8c1c9f
Wiktor0016 <sobowiec@onet.pl>
2017-11-24 08:46:01 +0100

    cw

--- python/minmax.py
+++ python/minmax.py
@@ -1,29 +1,50 @@
-def losuj(ileliczb, maksliczb):
-    liczby = []  # pusta lista
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  minmax.py
 
-    ile = 0  # ilość unikalnych liczb
+import random
+
+def losuj(ileliczb, maksliczb):
+    liczby = []
+
+    ile = 0
     # for i in range(ileliczb):
     while ile < ileliczb:
         liczba = random.randint(0, maksliczb)
         if liczby.count(liczba) == 0:
             liczby.append(liczba)
             ile += 1
-    print(liczby)
+
+    #  print(liczby)
     return liczby
 
 
-def miniumum(lista):
-    """wyszukiwanie minimum"""
+def minimum(lista):
     min = lista[0]
     for i, el in enumerate(lista):
-        print(i, el)
-    return 0
+        if el < min:
+            min = el
+    return min
+
+
+def maximum(lista):
+    max = lista[0]
+    for i, el in enumerate(lista):
+        if el > max:
+            max = el
+    return max
 
 def main(args):
-    lista = losuj(20, 50)
-        print("Min:", minimum(lista)
+    lista = losuj (20, 50)
+    assert minimum([7, 5, 2, 1, 7]) == 1
+    assert maximum([7, 5, 2, 1, 7]) == 7
+    print ("Min:", minimum(lista))
+    print ("Max: ", maximum(lista))
+    print (lista)
     return 0
 
-main() __name__ == '__main__':
-        import sys
-            ys.exit(main(sys.argv))
+
+if __name__ == '__main__':
+    import sys
+sys.exit(main(sys.argv))
