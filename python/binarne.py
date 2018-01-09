94bffde8ed93dcaa7a28653976322fcd8af23a42
Wiktor0016 <sobowiec@onet.pl>
2018-01-09 10:16:31 +0100

    binarne

--- python/binarne.py
+++ python/binarne.py
@@ -9,26 +9,26 @@ def wyszukiwanie_liniowe(l, e):
     for i in range(0, len(l)):
         if l[i] == e:
             return i
-    return -1  # liczba mniejsza od zera jest poprawna
+    return -1
 
 
-def wyszukiwanie_binarne(l, e):
-    lewy, prawy = 0, len(l) - 1  # 0 bo to krańcvowe po lewej stronie
+def wyszukiwanie_bin_iter(l, e):
+    lewy, prawy = 0, len(l) - 1
     while lewy < prawy:
         srodek = floor((lewy + prawy) / 2)
-        print(srodek)
         if e <= l[srodek]:
             prawy = srodek
         else:
             lewy = srodek + 1
 
     if l[lewy] == e:
-        return lewy:  # element znaleziono
+        return lewy  # element znaleziono
 
-    return -1  # olementu nie znaleziono
+    return -1  # elementu nie znaleziono
 
 
 def wyszukiwanie_bin_rek(lewy, prawy, lista, el):
+
     if lewy > prawy:
         return -1  # elementu nie znaleziono
 
@@ -39,15 +39,19 @@ def wyszukiwanie_bin_rek(lewy, prawy, lista, el):
     if el < lista[srodek]:
         return wyszukiwanie_bin_rek(lewy, srodek - 1, lista, el)
     else:
-        return wyszukiwanie_bin_rek(prawy, srodek + 1, prawy, lista, el)
+        return wyszukiwanie_bin_rek(srodek + 1, prawy, lista, el)
 
 
 def main(args):
     lista = [4, 3, 7, 0, 2, 3, 1, 9, -4]
     el = 2
-    print(wyszukiwanie_liniowe(lista, el))
+    print (wyszukiwanie_liniowe(lista, el))
     lista.sort()
-    print(wyszukiwanie_binarne(lista, el))
+    print(lista)
+    print (wyszukiwanie_bin_iter(lista, el))
+    print (wyszukiwanie_bin_rek(0, len(lista) - 1, lista, el))
+
+    return 0
 
 
 if __name__ == '__main__':
